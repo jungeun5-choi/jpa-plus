1fe959b9613ee2257d348868ceec51f006372a71
jungeun-choi <jungeun5.choi@gmail.com>
2023-07-26 17:41:46 +0900

    RawJPA 개발 기초 실습

---
+++ src/main/java/me/study/jpa/user/User.java
@@ -0,0 +1,73 @@
+package me.study.jpa.user;
+
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.ToString.Exclude;
+
+// lombok
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 외부에서 bean 생성자를 생성하지 못하도록
+@ToString //
+
+// jpa
+@Entity
+@Table(name = "users")
+public class User {
+    /**
+     * 컬럼 - 연관관계 컬럼을 제외한 컬럼을 정의합니다.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+
+    /**
+     * 생성자 - 약속된 형태로만 생성가능하도록 합니다.
+     */
+    @Builder
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "street", column = @Column(name = "home_street"))
+    })
+    private Address address;
+
+
+
+
+    /**
+     * 연관관계 - Foreign Key 값을 따로 컬럼으로 정의하지 않고 연관 관계로 정의합니다.
+     */
+
+
+    /**
+     * 연관관계 편의 메소드 - 반대쪽에는 연관관계 편의 메소드가 없도록 주의합니다.
+     */
+
+
+    /**
+     * 서비스 메소드 - 외부에서 엔티티를 수정할 메소드를 정의합니다. (단일 책임을 가지도록 주의합니다.)
+     */
+    public void updateUserName(String username) {
+        this.username = username;
+    }
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
+}
+
